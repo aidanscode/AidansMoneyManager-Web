4a3791304af3b3d45588eb7a24b88ae14d252a6f
Zee <amurphey7@gmail.com>
2024-07-29 10:37:57 -0400

    Add react router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { createBrowserRouter, RouterProvider, Link } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: (
+      <div>
+        <span>Hello, world :)</span>
+        <Link to='/test'>Click me</Link>
+      </div>
+    )
+  },
+  {
+    path: '/test',
+    element: <div>Testing route</div>
+  }
+])
+
 function App() {
-  return <h1 className='text-primary'>Hello, world</h1>
+  return <RouterProvider router={router} />
 }
 
 export default App
